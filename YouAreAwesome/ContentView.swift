bac21188143bec8461f14cb446d7d10c59412246
Zo-hack <enzoarantes2000@gmail.com>
2025-01-24 13:39:43 -0500

    Ch. 1.13 from Prof. Gallaugher's SwiftUI Course, integrating sound files, guard let functions, do-try-catch functions, and more!

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var lastMessageNumber = -1
     @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 9
+    let numberOfSounds = 6
     var body: some View {
         
         VStack {
@@ -23,6 +28,8 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 100)
                 .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Spacer()
             
             Image(imageName)
                 .resizable()
@@ -39,6 +46,7 @@ struct ContentView: View {
                                 "You make me Smile!",
                                 "You are Great", "You are Fantastic!",
                                 "Fabulous? That's You!"]
+                // Randomizes withut repeating the message
                 var messageNumber: Int
                 repeat {
                     messageNumber = Int.random(in: 0...messages.count-1)
@@ -46,12 +54,32 @@ struct ContentView: View {
                 message = messages[messageNumber]
                 lastMessageNumber = messageNumber
                 
+                // Randomizes withut repeating the image
                 var imageNumber: Int
                 repeat {
-                    imageNumber = Int.random(in: 0...9)
+                    imageNumber = Int.random(in: 0...(numberOfImages-1))
                 } while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
                 lastImageNumber = imageNumber
+                
+                // Randomizes withut repeating the sound
+                var soundNumber: Int
+                repeat {
+                    soundNumber = (Int.random(in: 0...numberOfSounds-1))
+                } while soundNumber == lastSoundNumber
+                lastSoundNumber = soundNumber
+                let soundName = "sound\(soundNumber)"
+                
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("😡 Could not read file named \(soundName)")
+                    return
+                }
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+                }
             }
             
             .buttonStyle(.borderedProminent)
